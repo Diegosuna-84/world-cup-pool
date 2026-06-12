8acce5c0035c4503788fe379c5ce023110471f65
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-06-11 23:44:25 -0400

    switch news to newsdata.io

--- api/news.js
+++ api/news.js
@@ -1,18 +1,16 @@
 export default async function handler(req, res) {
   const response = await fetch(
-    'https://feeds.bbci.co.uk/sport/football/rss.xml'
+    `https://newsdata.io/api/1/news?apikey=${process.env.NEWSDATA_API_KEY}&q=World+Cup+2026&language=en&category=sports`
   )
-  const text = await response.text()
+  const data = await response.json()
 
-  const items = [...text.matchAll(/<item>([\s\S]*?)<\/item>/g)].map(match => {
-    const item = match[1]
-    const title = item.match(/<title><!\[CDATA\[(.*?)\]\]><\/title>/)?.[1] || item.match(/<title>(.*?)<\/title>/)?.[1] || ''
-    const url = item.match(/<link>(.*?)<\/link>/)?.[1] || ''
-    const publishedAt = item.match(/<pubDate>(.*?)<\/pubDate>/)?.[1] || ''
-    const urlToImage = item.match(/url="(.*?)"/)?.[1] || null
+  const articles = (data.results || []).map(article => ({
+    title: article.title,
+    url: article.link,
+    publishedAt: article.pubDate,
+    urlToImage: article.image_url
+  }))
 
-    return { title, url, publishedAt, urlToImage }
-  })
-
-  res.status(200).json({ articles: items })
+  res.status(200).json({ articles })
+}
 }
