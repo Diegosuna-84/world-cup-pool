e464e59f19d441db6898203fe77d5fa2d1b89d8c
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-06-16 14:36:13 -0400

    revert: restore n8n as match data source

--- src/services/prediction.js
+++ src/services/prediction.js
@@ -1,4 +1,6 @@
+const N8N_BASE = 'https://kadowebstudio.app.n8n.cloud/webhook'
+
 export const getMatches = async () => {
-  const response = await fetch('/api/matches')
+  const response = await fetch(`${N8N_BASE}/get-matches`)
   return response.json()
 }
