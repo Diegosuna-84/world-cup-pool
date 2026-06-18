4911e978dde3c87a6e0aacd65247b955fe8593f0
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-06-18 00:26:15 -0400

    Update match fetching service

--- src/services/prediction.js
+++ src/services/prediction.js
@@ -1,6 +1,4 @@
-const N8N_BASE = 'https://kadowebstudio.app.n8n.cloud/webhook'
-
 export const getMatches = async () => {
-  const response = await fetch(`${N8N_BASE}/get-matches`)
+  const response = await fetch('/api/matches')
   return response.json()
 }
